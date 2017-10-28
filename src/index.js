736f4a715a4d0692788e5b3417fd8fdd00e41fb9
Dipayan Deb <chotumotu11@gmail.com>
2017-10-28 12:45:06 +0530

    UI Complete

--- src/index.js
+++ src/index.js
@@ -2,17 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import {Nav , Navbar  , NavItem , Form , FormGroup , ControlLabel , FormControl , Button , Grid , Row , Col } from 'react-bootstrap';
+import {Nav , Navbar  , NavItem , Form , FormGroup , ControlLabel , FormControl , Button , Grid , Row , Col , Modal , Table } from 'react-bootstrap';
 import './index.css';
 
 class SubmitForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = {title: "",text: "", tags: 0};
+        this.state = {title: "",text: "", tags: 0 , showModal: false};
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleOnChange = this.handleOnChange.bind(this);
+        this.getInitialState = this.getInitialState.bind(this);
+        this.close = this.close.bind(this);
+        this.open = this.open.bind(this);
     }
 
+    getInitialState() {
+      return {showModal: false};
+    }
+
+    close(){
+     this.setState({showModal: false}); 
+    }
+
+    open(){
+      this.setState({showModal: true});
+    }
 
     handleSubmit(event){
       if(this.state.title!="" && this.state.text!="" ){
@@ -34,31 +48,43 @@ class SubmitForm extends React.Component {
       }
     }
     render() {
+
         return (
-          <Form onSubmit={this.handleSubmit} inline>
-            <FormGroup  controlId="formInlineName">
-              <ControlLabel>Title</ControlLabel>
-              {' '}
-              <FormControl name="title" type="text" onChange={this.handleOnChange} value={this.state.title} />
-            </FormGroup>
-            {' '}
-            <FormGroup>
-              <ControlLabel>Description </ControlLabel>
-              {' '}
-              <FormControl componentClass="textarea" value={this.state.text} name="description" type="text" onChange={this.handleOnChange} />
-            </FormGroup>
-            {' '}
-            <FormGroup>
-              <ControlLabel>Select Tag</ControlLabel>
-              {' '}
-              <FormControl componentClass="select" name="tags" onChange={this.handleOnChange}  >
-                {this.props.tags.map((x,index) => (<option key={index} value={index}>{x} </option>))}
-              </FormControl>
-            </FormGroup>
-            <p>
-              <Button bsStyle="success" type="submit">Submit</Button>
-            </p>
-          </Form>
+          <div>
+            <p className="center"> <Button bsStyle="primary" bsSize="large" onClick={this.open}>New Issue</Button></p>
+            <Modal show={this.state.showModal} onHide={this.close}>
+              <Modal.Header closeButton>
+                <Modal.Title> Create New Issue</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>  
+                <Form onSubmit={this.handleSubmit}>
+                  <FormGroup  controlId="formInlineName">
+                    <ControlLabel>Title</ControlLabel>
+                    {' '}
+                    <FormControl name="title" type="text" onChange={this.handleOnChange} value={this.state.title} />
+                  </FormGroup>
+                  {' '}
+                  <FormGroup>
+                    <ControlLabel>Description </ControlLabel>
+                    {' '}
+                    <FormControl componentClass="textarea" value={this.state.text} name="description" type="text" onChange={this.handleOnChange} />
+                  </FormGroup>
+                  {' '}
+                  <FormGroup>
+                    <ControlLabel>Select Tag</ControlLabel>
+                    {' '}
+                    <FormControl componentClass="select" name="tags" onChange={this.handleOnChange}  >
+                      {this.props.tags.map((x,index) => (<option key={index} value={index}>{x} </option>))}
+                    </FormControl>
+                  </FormGroup>
+                  <Button bsStyle="success" type="submit">Submit</Button>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.close}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         );
     }
 }
@@ -80,7 +106,7 @@ class DisplayListAndForm extends React.Component {
 
     <div>
       <SubmitForm add={this.props.add} tags={this.props.tags} />
-      <h2>ALL Issues</h2>
+      <h2>Issue List</h2>
       <Row>{this.props.value.map((li,index) => <Col  xs={6} sm={3} key={index}> <div className="design"> <p className="paradesign"> Title {li.title} </p> <p className="para2design"> Tags {this.props.tags[li.tags]} <p className="para3"><Button id={index} bsStyle="primary" onClick={this.assignDetails} >View</Button> </p> </p> </div> </Col>) } </Row>
     </div>
     );
@@ -91,9 +117,24 @@ class SearchByTag extends React.Component {
 
   constructor(props){
     super(props);
-    this.state={value: ""};
+    this.state={value: "",showModal: false};
     this.addnewtag = this.addnewtag.bind(this);
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.getInitialState = this.getInitialState.bind(this);
+    this.close = this.close.bind(this);
+    this.open = this.open.bind(this);
+  }
+
+  getInitialState() {
+    return { showModal: false };
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
   }
 
   addnewtag(e){
@@ -109,18 +150,25 @@ class SearchByTag extends React.Component {
   render(){
     return (
       <div>
-        <h2>Add new tag</h2>
-        <Form onSubmit={this.addnewtag} inline>
-          <FormGroup>
-            <ControlLabel>New Tag</ControlLabel>
-            {' '}
-            <FormControl type="text" name="addtags" onChange={this.handleOnChange} value={this.state.value} />
-          </FormGroup>
-          {' '}
-          <Button bsStyle="warning" type="submit">Submit</Button>
-        </Form>
+        <p className="center"> <Button bsStyle="primary" bsSize="large" onClick={this.open}>New Tag</Button></p>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title> New Tag</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.addnewtag} >
+              <FormGroup>
+                <ControlLabel>Name</ControlLabel>
+                {' '}
+                <FormControl type="text" name="addtags" onChange={this.handleOnChange} value={this.state.value} />
+              </FormGroup>
+              {' '}
+              <Button bsStyle="warning" type="submit">Submit</Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
         <h2> All Tags </h2>
-        {this.props.tags.map((x,index) => (<li key={index}> {x} </li>))}
+        <Row> {this.props.tags.map((x,index) => (<Col  xs={6} sm={3}  key={index}> <div className="design center"> {x} </div> </Col>))} </Row>
       </div>
     );
   }
@@ -132,14 +180,24 @@ class DisplayDetails extends React.Component{
     const myobject = this.props.value[index];
 
     return (
-    <div>
-      <h2> Issue Details </h2>
-      <h3>Title</h3>
-      <p>{myobject.title}</p>
-      <h3>Description</h3>
-      <p>{myobject.text} </p>
-      <h3>Tags </h3>
-      <p>{this.props.tags[myobject.tags]} </p>
+    <div> 
+      <h1>Issue Details</h1>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td> {myobject.title} </td>
+            <td>{myobject.text} </td>
+            <td>{this.props.tags[myobject.tags]} </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
       ); 
   }
