d6f6fb6a59bb66e33c9e446032a0dbe50ab2225d
Dipayan Deb <chotumotu11@gmail.com>
2017-11-06 10:51:55 +0530

    small content added

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ class DisplayListAndForm extends React.Component {
 
     <div>
       <SubmitForm add={this.props.add} tags={this.props.tags} />
-      <h2>Issue List</h2>
+      <h2>Issue List</h2><small>(Click on Title to view details.)</small>
       <ul>
         {this.props.value.map((li,index) => <li key={index} className="blackborder"> <h3 id={index}  onClick={this.assignDetails} >{li.title}</h3> Tags: {li.tags.map((pi,ind) => <span style={valstyle[index][ind]} key={ind} className="spans">{pi}</span> )} </li> ) }
       </ul>
